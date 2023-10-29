1d0cf5d5465bad55c2427b193d7ec25524fd5801
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-10-29 12:20:24 +0200

    Create api_integration.py

---
+++ api_integration.py
@@ -0,0 +1,40 @@
+# Python API Integration Project
+# Objective:
+# The objective of this project is to provide students with hands-on experience in creating,
+# a communication interface with an external API using Python.
+# Students will learn how to make HTTP requests, handle API responses, and work with data retrieved from the API.
+#
+# Requirements:
+# Basic knowledge of Python programming.
+# Familiarity with HTTP concepts (GET and POST requests).
+# Understanding of JSON data format.
+# Task Description: In this project,
+# students will develop a Python program that communicates with a chosen public API.
+# You can let the students select an API of their choice, but some suggestions include:
+#
+# Weather data API (e.g., OpenWeatherMap)
+# RESTful APIs for a social media platform (e.g., Twitter or Reddit)
+# Currency exchange rate API (e.g., Open Exchange Rates)
+# Public transportation data (e.g., using Google Maps API)
+# Here are the steps students should follow to complete the project:
+# Choose an API: You can choose a public API that interests you or matches the goals of the project.
+# Make sure the API you choose offers good documentation.
+#
+# Research and understand the API: Students should thoroughly read the API documentation to understand its endpoints,
+# request methods, and data format (usually JSON).
+#
+# Create a Python script: Write Python code to make a GET request to the chosen API's endpoint.
+#
+# Handle the API response, including error handling for potential issues like rate limiting or network errors.
+#
+# Parse the response data (usually in JSON format) and extract relevant information.
+#
+# Implement a use case: Students should create a specific use case for their API integration.
+# For example, if using a weather API,
+# they can write a program that fetches the current weather for a user-defined location and displays it.
+#
+# User-friendly interface: Depending on the use case,
+# students can create a simple text-based or graphical user interface to interact with the API.
+#
+# Testing and debugging: Students should thoroughly test their
+# Python script and handle any errors or issues that may arise.
